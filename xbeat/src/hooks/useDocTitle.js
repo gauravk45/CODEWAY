ef507b4a672b5fe12a7be546b78f6462ea4a438d
gauravk45 <gauravkathiriya3590@gmail.com>
2024-02-12 19:25:31 +0530

    Update

---
+++ xbeat/src/hooks/useDocTitle.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+const useDocTitle = (title) => {
+    useEffect(() => {
+        if (title) {
+            document.title = `${title} - X-Beat`;
+        } else {
+            document.title = 'X-Beat | The Perfect Audio Store';
+        }
+    }, [title]);
+    return null;
+};
+
+export default useDocTitle;
